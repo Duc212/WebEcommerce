489bd7a72127bdc2aa5cbfb2360a761a3a4f0350
Duc212 <63503940+Duc212@users.noreply.github.com>
2025-04-11 00:06:22 +0700

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/WebEcommerce/QuanLyDuAn/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuanLyDuAn.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
